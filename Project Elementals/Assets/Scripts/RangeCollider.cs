23c8de93a778ef26bf31cd6bb1fbff4c94c53cd6
Siddharth Menon <112028129+sm2947@users.noreply.github.com>
2023-04-16 18:48:29 -0400

    Further Animation fixes

--- Project Elementals/Assets/Scripts/RangeCollider.cs
+++ Project Elementals/Assets/Scripts/RangeCollider.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class RangeCollider : MonoBehaviour
 {
+
+    [SerializeField] private int abilityReferenceNumber;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Collision Detected");
+        if (collision.gameObject.tag == "Enemy")
+            transform.parent.gameObject.GetComponent<Character>().AbilityHit(abilityReferenceNumber,collision.gameObject);
     }
 }
