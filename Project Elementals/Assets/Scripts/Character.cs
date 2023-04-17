02caab4c3abe1a8251f1121d6b4368ef17cef9b3
Siddharth Menon <112028129+sm2947@users.noreply.github.com>
2023-04-16 21:06:43 -0400

    Forgot to push the actual change lmao

--- Project Elementals/Assets/Scripts/Character.cs
+++ Project Elementals/Assets/Scripts/Character.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class Character : MonoBehaviour
+
+
+
+
+
+
+public class Character : MonoBehaviour 
 {
     private Rigidbody2D rgb;
+
 
 
     [SerializeField] private float accel;
     [SerializeField] private float maxVelocity;
     [SerializeField] private float decel;
+    public GameObject boxColliderPrefab;
+    public GameObject circleColliderPrefab;
+    private bool isWind = true;
+    private int equippedItem1;
+    private int equippedItem2;
+    [SerializeField]public List<Ability> abilities = new List<Ability>();
+    
     // Start is called before the first frame update
     void Start()
     {
         rgb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -39,7 +55,7 @@ public class Character : MonoBehaviour
 
         if(Input.GetMouseButtonDown(1))//Right Click
         {
-            Debug.Log(1);
+            UseAbility();
         }
         if(Input.GetMouseButtonDown(0)) //LeftClick
         {
@@ -51,8 +67,11 @@ public class Character : MonoBehaviour
         }
        
     }
-
     public void UseAbility()
+    {
+
+    }
+    public void AbilityHit(int abilityReferenceNumber, GameObject enemyHit)
     {
 
     }
@@ -65,5 +84,95 @@ public class Character : MonoBehaviour
     public void Switch()
     {
 
+    }
+}
+
+[System.Serializable]
+public class Ability
+{
+    public string name;
+    public int colliderType; // 0 - Rectangle, anything else - Circular
+    public float xOffset;
+    public float yOffset;
+    public float xSize;
+    public float ySize;
+
+    public Ability(string n, int cType, float xO, float yO, float xS, float yS)
+    {
+        name = n;
+        colliderType = cType;
+        xOffset = xO;
+        yOffset = yO;
+        xSize = xS;
+        ySize = yS;
+    }
+}
+
+public class BuildColliders:EditorWindow
+{
+
+
+    [MenuItem("Window/Generate Colliders")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(BuildColliders));
+    }
+
+   
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Run Function"))
+        {
+            CreateColliders();
+            Debug.Log("Got here");
+        }
+    }
+   
+    private GameObject player;
+   [SerializeField] public List<Ability> abilities;
+    public GameObject boxColliderPrefab;
+    public GameObject circleColliderPrefab;
+    private BoxCollider2D boxColliderReference;
+    private CircleCollider2D circleColliderReference;
+    private GameObject cloneReference;
+
+    private void OnEnable()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        boxColliderPrefab = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().boxColliderPrefab;
+        circleColliderPrefab = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().circleColliderPrefab;
+        Debug.Log("Got here tho");
+    }
+
+    private void CreateColliders()
+    {
+        Debug.Log("Got here");
+        abilities = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().abilities;
+        foreach(Transform t in player.transform)
+        {
+            Debug.Log(t.name);
+            if (t.name.Contains("Range"))
+                DestroyImmediate(t.gameObject);
+        }
+        foreach (Ability a in abilities)
+        {
+            if(a.colliderType == 0)
+            {
+                cloneReference = GameObject.Instantiate(boxColliderPrefab, player.transform);
+                boxColliderReference = cloneReference.GetComponent<BoxCollider2D>();
+                boxColliderReference.size = new Vector2(a.xSize, a.ySize);
+                boxColliderReference.offset = new Vector2(a.xOffset, a.yOffset);
+
+            }
+            else
+            {
+                cloneReference = GameObject.Instantiate(circleColliderPrefab, player.transform);
+                circleColliderReference = cloneReference.GetComponent<CircleCollider2D>();
+                circleColliderReference.radius = a.xSize;
+                circleColliderReference.offset = new Vector2(a.xOffset, a.yOffset);
+            }
+            cloneReference.name = a.name + "Range";
+        }
+        
     }
 }
